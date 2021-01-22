7d17546b420a7346fc5728ef6046b55517ef920e
Ronak Agrawal <66140194+ronak-ag19@users.noreply.github.com>
2021-01-22 12:49:58 +0530

    Update Returns.py

--- Returns.py
+++ Returns.py
@@ -4,8 +4,8 @@ import datetime as dt
 
 stock_symbol=input("enter the Stock Symbol: ")
 
-start_date=str(int(dt.datetime(2018,10,1).timestamp())+19800) 
-end_date=str(int(dt.datetime(2019,9,30).timestamp())+19800)
+start_date=str(int(dt.datetime(2019,11,1).timestamp())+19800) 
+end_date=str(int(dt.datetime(2020,12,29).timestamp())+19800)
 
 freq=['1d','1wk','1mo']    
 event="history"
@@ -18,21 +18,25 @@ for i in freq:
     url_list.append(url)
     
 factor="Close"                   
-new_column=["Daily Returns (%)","Weekly Returns (%)","Monthly Returns (%)"]
-table=[daily_data,weekly_data,monthly_data]
+new_column=["Daily Freq","Weekly Freq","Monthly Freq"]
+#table=[daily_data,weekly_data,monthly_data]
     
 daily_data=pd.read_csv(url_list[0])
-daily_data[new_column[0]]=(table[0][factor]/table[0][factor].shift(1)-1)*100
+daily_data[new_column[0]]=(daily_data[factor]/daily_data[factor].shift(1)-1)*100
 
 weekly_data=pd.read_csv(url_list[1])
-weekly_data[new_column[1]]=(table[1][factor]/table[1][factor].shift(1)-1)*100
+weekly_data[new_column[1]]=(weekly_data[factor]/weekly_data[factor].shift(1)-1)*100
 
 monthly_data=pd.read_csv(url_list[2])
-monthly_data[new_column[2]]=(table[2][factor]/table[2][factor].shift(1)-1)*100
+monthly_data[new_column[2]]=(monthly_data[factor]/monthly_data[factor].shift(1)-1)*100
     
 data=pd.concat([daily_data,weekly_data,monthly_data],axis=1)
 data=round(data[new_column].describe().iloc[[1,2,3,7]],2)
-
+data.loc['Return/Risk']=data.loc['mean']/data.loc['std']
+data = data.rename(index={'mean': 'Mean Return (%)',
+                         'std':'Mean Risk(%)',
+                         'min':'Min Return(%)',
+                         'max':'Max Return(%)'})
 def color_negative_red(val):
     color = 'red' if val < 0 else 'black'
     return 'color: %s' % color
